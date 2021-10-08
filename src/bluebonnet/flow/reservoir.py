3967327e4446061576476730eff2d29c70552718
Frank Male <frmale@utexas.edu>
2021-10-07 21:59:45 -0500

    feat: real gas simulation, relative permeabilities

--- src/bluebonnet/flow/reservoir.py
+++ src/bluebonnet/flow/reservoir.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import scipy as sp
 import numpy.typing as npt
+from collections import namedtuple
 from bluebonnet.fluids.fluid import Fluid
 from bluebonnet.fluids.gas import pseudopressure_Hussainy
 
@@ -92,9 +93,7 @@ class IdealReservoir:
         pp = self.pseudopressure[:, :3]
         dp_dx = (-pp[:, 2] + 4 * pp[:, 1] - 3 * pp[:, 0]) * h_inv * 0.5
         cumulative = sp.integrate.cumulative_trapezoid(dp_dx, self.time, initial=0)
-        self.recovery = cumulative * (
-            1 - self.pressure_fracface / self.pressure_initial
-        )
+        self.recovery = cumulative * self.fvf_scaling()
         return self.recovery
 
     def _build_matrix(self, pseudopressure: npt.NDArray[np.float64], mesh_ratio: float):
@@ -124,3 +123,114 @@ class IdealReservoir:
     ) -> npt.NDArray[np.float64]:
         "Calculate scaled diffusivity"
         return np.ones_like(pseudopressure)
+
+    def fvf_scaling(self):
+        return 1 - self.pressure_fracface / self.pressure_initial
+
+
+class SinglePhaseReservoir(IdealReservoir):
+    def alpha_scaled(
+        self, pseudopressure: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        "Calculate scaled diffusivity"
+        alpha = self.fluid.alpha
+        return alpha(pseudopressure) / alpha(1)
+
+    def fvf_scaling(self):
+        raise NotImplementedError  # TODO: fvf for fracface, initial reservoir
+
+
+class MultiPhaseReservoir(IdealReservoir):
+    def simulate(self, time: npt.NDArray[np.float64]):
+        """
+        Calculate simulation pressure over time
+
+        Parameters
+        ----------
+        time: array of times to solve for pressure
+        """
+        raise NotImplementedError  # TODO: saturation changes
+        self.time = time
+        x = np.linspace(0, 1, self.nx)
+        dx_squared = (x[1] - x[0]) ** 2
+        pseudopressure = np.empty((len(time), self.nx))
+        pseudopressure[0, :] = 1
+        pseudopressure[0, 0] = 0
+        for i in range(time.shape[0] - 1):
+            mesh_ratio = (time[i + 1] - time[i]) / dx_squared
+            b = pseudopressure[i]
+            a_matrix = self._build_matrix(b, mesh_ratio)
+            pseudopressure[i + 1], info = sp.sparse.linalg.bicgstab(a_matrix, b)
+        self.pseudopressure = pseudopressure
+
+    def alpha_scaled(
+        self,
+        pseudopressure: npt.NDArray[np.float64],
+        So: npt.NDArray[np.float64],
+        Sg: npt.NDArray[np.float64],
+        Sw: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
+        "Calculate scaled diffusivity"
+        alpha = self.fluid.alpha
+        return alpha(pseudopressure, So, Sg, Sw) / alpha(1)
+
+    def fvf_scaling(self):
+        raise NotImplementedError
+
+
+RelPermParams = namedtuple(
+    "RelPermParams", "n_o n_g n_w S_or S_wc S_gc k_ro_max k_rw_max k_rg_max"
+)
+
+
+def relative_permeabilities(
+    saturations: npt.NDArray,
+    params: RelPermParams,
+) -> npt.NDArray:
+    """
+    Brooks-Corey power-law relative permeability
+
+    Parameters
+    ----------
+    saturations: numpy record array with columns for So, Sg, Sw
+    params: RelPermParams with Corey exponents, residual saturations, and max relative permeabilities
+
+    Returns
+    -------
+
+    """
+    assert (
+        np.abs(sum(v for v in saturations.values()) - 1) < 1e-3
+    ), "Saturations must sum to 1"
+    assert max(params.n_o, params.n_g, params.n_w) <= 6, "Exponents must be less than 6"
+    assert min(params.n_o, params.n_g, params.n_w) >= 1, "Exponents must be at least 1"
+    assert (
+        min(params.S_or, params.S_wc, params.S_gc) >= 0
+    ), "Critical saturations must be at least 0"
+    assert (
+        max(params.S_or, params.S_wc, params.S_gc) <= 1
+    ), "Critical saturations must be less than 1"
+    assert (
+        min(params.k_ro_max, params.k_rw_max, params.k_rg_max) >= 0
+    ), "Max relative permeability must be at least 0"
+    assert (
+        max(params.k_ro_max, params.k_rw_max, params.k_rg_max) <= 1
+    ), "Max relative permeability must be less than 1"
+
+    denominator = 1 - params.S_or - params.S_wc - params.S_gc
+    k_o = (
+        params.k_ro_max
+        * ((saturations["So"] - params.S_or) / denominator) ** params.n_o
+    )
+    k_w = (
+        params.k_rw_max
+        * ((saturations["Sw"] - params.S_wc) / denominator) ** params.n_w
+    )
+    k_g = (
+        params.k_rg_max
+        * ((saturations["Sg"] - params.S_gr) / denominator) ** params.n_g
+    )
+    k_rel = np.array(
+        [k_o, k_w, k_g], dtype=[(i, np.float64) for i in ("k_o", "k_w", "k_g")]
+    )
+    return k_rel
